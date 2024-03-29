acf9c09ce4be9f80714001078262c8fea137cf93
aureliony <39163684+aureliony@users.noreply.github.com>
2024-03-29 18:20:05 +0800

    Improve code style

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -41,7 +41,6 @@ public class UiManager implements Ui {
 
         try {
             mainWindow = new MainWindow(primaryStage, logic);
-
         } catch (Throwable e) {
             logger.severe(StringUtil.getDetails(e));
             showFatalErrorDialogAndShutdown("Fatal error during initializing", e);
