ea199d2e099f9efbbef383ee5d92629c26fc93fc
aureliony <39163684+aureliony@users.noreply.github.com>
2024-04-02 21:24:16 +0800

    Fix checkstyle

--- src/main/java/seedu/address/commons/exceptions/CommandHistoryException.java
+++ src/main/java/seedu/address/commons/exceptions/CommandHistoryException.java
@@ -1,5 +1,8 @@
 package seedu.address.commons.exceptions;
 
+/**
+ * Represents an error that occurred while navigating the command history.
+ */
 public class CommandHistoryException extends Exception {
 
     public CommandHistoryException(String message) {
