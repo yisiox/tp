664cc3fa2d093748d78628ba97027280792eb94d
bryanyee33 <bryanyee33@gmail.com>
2024-04-05 09:07:12 +0800

    Fix excess backslash

--- src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
@@ -73,7 +73,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100, false, 0.75));
-        userPrefs.setAddressBookFilePath(Paths.get("data\\\\assetbook.json"));
+        userPrefs.setAddressBookFilePath(Paths.get("data\\assetbook.json"));
         return userPrefs;
     }
 
